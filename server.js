949bee1681aaa5d97a486a85cf63cc07347f85c0
Kadeen <nel16064@byui.edu>
2022-05-06 13:55:50 -0600

    Added files

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
 const express = require ('express'); //import the library
-
+const bodyParser =require ('body-parser'); //body perser is called middleware
+const port =3000;
 const app = express (); //use the library
 
-app.listen(3000, () =>{console.log("listening...")});   //listening
+app.use (bodyParser.json()); //use the middleware (call it before anything else happens on each request)
 
-app.get ('/', (request,response)=>{response.send ("Hello")}); //respond
+app.listen(port, () =>
+
+{
+    console.log("listening on port: "+port);
+
+})   //listening  
+
+
+
+app.post ('/login', (request, response)=> {//a post is when a client sends new information to an API
+const loginRequest = request.body;
+if (loginRequest.userName=="jann@yahoo.com" && loginRequest.password=="Operatingsystem1!")
+{
+    response.status (200); //200 means Ok
+    response.send ("Welcome");
+} //end if statement
+
+    else
+        {
+            response.status (401)   //401 means unauthorized
+            response.send ("Unathourized");
+ 
+        }  //end of else statement
+
+
+});
+
+app.get('/', (request, response)=>
+{response.send ("Hello"); // a response is when the API gives the 
+
+}
+)
