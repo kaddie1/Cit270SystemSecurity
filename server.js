8241a0cf264912fb327bc0138d07fd0904b59496
Kadeen <nel16064@byui.edu>
2022-05-13 13:56:15 -0600

    Redis

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require ('express'); //import the library
 const bodyParser =require ('body-parser'); //body perser is called middleware
 const port =3000;
 const app = express (); //use the library
+const md5 = require ('md5');
+const redis = require ('redis');
+const redisClient = redis.createClient();
 
 app.use (bodyParser.json()); //use the middleware (call it before anything else happens on each request)
 
@@ -14,7 +17,16 @@ app.listen(port, () =>
 
 
 
-app.post ('/login', (request, response)=> {//a post is when a client sends new information to an API
+app.post ('/login', async (request, response)=> {//a post is when a client sends new information to an API
+const requestHashedPassword= md5 (request.body.password);
+const redisHashedPassword= await redisClient.hGet('password', request.body.userName);
+const loginRequest = request.body;
+console.log ("Request Body", JSON.stringify(request.body));
+
+//search database for username, and retrieve current password  
+
+//compare the hashed version of the password that was sent with the hashed version from the database
+
 const loginRequest = request.body;
 if (loginRequest.userName=="jann@yahoo.com" && loginRequest.password=="Operatingsystem1!")
 {
