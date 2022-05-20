11f92f0f5a7d2e53cf27e12fddd5b762a4a58aee
Kadeen <nel16064@byui.edu>
2022-05-20 13:50:13 -0600

    API connecting to Redis

--- server.js
+++ server.js
@@ -1,51 +1,73 @@
-const express = require ('express'); //import the library
-const bodyParser =require ('body-parser'); //body perser is called middleware
+const express = require ('express');
 const port =3000;
-const app = express (); //use the library
+const app =express();
 const md5 = require ('md5');
-const redis = require ('redis');
-const redisClient = redis.createClient();
+const bodyParser = require ('body-parser');
+const {createClient} = require ('redis');
+const redisClient= createClient(
+{
+socket:
+{
+    port:6379,
+    host: "127.0.0.1"
+}
+
+} //end const redisClient
+
+); // this creates a connection to redis database
+
 
 app.use (bodyParser.json()); //use the middleware (call it before anything else happens on each request)
 
-app.listen(port, () =>
+app.listen(port, async() =>
 
 {
+    await redisClient.connect(); //creating a TCP socket with Redis
     console.log("listening on port: "+port);
 
-})   //listening  
+} // end await redisClient.connect ();
+)   //listening  
 
 
 
-app.post ('/login', async (request, response)=> {//a post is when a client sends new information to an API
-const requestHashedPassword= md5 (request.body.password);
-const redisHashedPassword= await redisClient.hGet('password', request.body.userName);
+const validatePassword =async (request, response) => 
+{
+//await redisClient.connect(); //creating a TCP socket with Redis
+const requestHashedPassword = md5 (request.body.password); //get the password from the body and hash it
+const redisHashedPassword= await redisClient.hmGet('password', request.body.userName); //read password from redis
 const loginRequest = request.body;
 console.log ("Request Body", JSON.stringify(request.body));
-
+console.log (redisHashedPassword);
 //search database for username, and retrieve current password  
 
 //compare the hashed version of the password that was sent with the hashed version from the database
-
-const loginRequest = request.body;
-if (loginRequest.userName=="jann@yahoo.com" && loginRequest.password=="Operatingsystem1!")
+if (requestHashedPassword==redisHashedPassword)
 {
     response.status (200); //200 means Ok
     response.send ("Welcome");
 } //end if statement
+else
+{
+    response.status (401)   //401 means unauthorized
+    response.send ("Unathourized");
 
-    else
-        {
-            response.status (401)   //401 means unauthorized
-            response.send ("Unathourized");
- 
-        }  //end of else statement
+}  //end of else statement
 
+} //end validate password
 
-});
-
-app.get('/', (request, response)=>
-{response.send ("Hello"); // a response is when the API gives the 
+app.get('/', (request, response)=>  
+{ //every time something calls your API that is a request
+    response.send ("Hello"); // a response is when the API gives the information requested
 
 }
 )
+
+app.post('/login', validatePassword);
+
+const signup =(request, response)
+{
+
+
+
+} //end const signup
+app.post ('/signup', signup);
